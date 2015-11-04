3d36e67d1b0265ba78810c1269f13ed722af2a08
Doug Beardsley <mightybyte@gmail.com>
2015-11-04 18:09:24 +0000

    Update templates

--- app/sandbox/template.hs
+++ app/sandbox/template.hs
@@ -1,14 +1,21 @@
-{-# LANGUAGE ConstraintKinds     #-}
-{-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE RecursiveDo         #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE ConstraintKinds       #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE RecordWildCards       #-}
+{-# LANGUAGE RecursiveDo           #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TupleSections         #-}
+{-# LANGUAGE TypeFamilies          #-}
 
 module Main where
 
 ------------------------------------------------------------------------------
 import           Control.Monad
+import           Data.Aeson
+import           Data.Aeson.TH
 import           Data.Map (Map)
 import qualified Data.Map as M
 import           Data.Monoid
@@ -16,6 +23,7 @@ import           Reflex
 import           Reflex.Dom
 import           Reflex.Dom.Contrib.Utils
 import           Reflex.Dom.Contrib.Widgets.EditInPlace
+import           Reflex.Dom.Contrib.Xhr
 ------------------------------------------------------------------------------
 import           HSnippet.Lib
 ------------------------------------------------------------------------------
